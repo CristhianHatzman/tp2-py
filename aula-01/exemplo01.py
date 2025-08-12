839ef99be8b765c9f72d1394c448495edb140248
Cristhian Hatzman <cristhianhatzman@gmail.com>
2025-08-11 22:03:20 -0300

    feat: adicionando primeira aula

---
+++ aula-01/exemplo01.py
@@ -0,0 +1,22 @@
+name='luiz'
+preço=125.12
+sexo='masculino'
+
+print(name)
+
+num1 = int(input("Digite um numero: "))
+num2 = int(input("Digite outro numero: "))
+resultado = print(num1 + num2)
+
+print(f"A soma dos numeros: {resultado}")
+
+altura = int(input("Digite a altura do triangulo"))
+base = int(input("Digite a base do triangulo"))
+area = (altura*base)/2
+
+print(f"A área do triângulo é: {area}")
+
+potencia = pow(num1,2)
+
+
+
